9a02d980f88f2dc0864991add88a02a1b6d731a4
준석 손 <thstjr0487@naver.com>
2022-03-07 02:17:38 +0900

    우선완료

--- Hellsys/Hell_UnitControls/HellsysLineGrid.xaml.cs
+++ Hellsys/Hell_UnitControls/HellsysLineGrid.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
+﻿using HellsysLib.Helpers;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Hellsys.Hell_UnitControls
 {
@@ -20,9 +11,31 @@ namespace Hellsys.Hell_UnitControls
     /// </summary>
     public partial class HellsysLineGrid : UserControl
     {
+
+        public List<string> ExcelList { get; set; }
+
         public HellsysLineGrid()
         {
             InitializeComponent();
+        }
+
+        private void btnLoadWorkLog_Click(object sender, RoutedEventArgs e)
+        {
+            GetExcelFiles();
+        }
+
+        private void GetExcelFiles()
+        {
+            ExcelList = Helper.XL.GetExcelFiles(@"T:\01_EPI생산팀\01_EPI제조파트\01_업무관리\01_업무인수인계일지\01_업무인수인계일지OP용");
+
+            if (ExcelList == null)
+                return;
+
+            lbxExcelFIleList = null;
+            foreach (var item in ExcelList)
+            {
+                lbxExcelFIleList.Items.Add(Path.GetFileName(item).ToString());
+            }
         }
     }
 }
